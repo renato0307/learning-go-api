9ad10d99b7fe62b4527288346546fd040e91d3a1
Renato Torres <renato.torres@outsystems.com>
2022-01-06 00:32:32 +0000

    wip: add authentication draft done

--- go.mod
+++ go.mod
@@ -5,30 +5,45 @@ go 1.17
 require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-playground/assert/v2 v2.0.1
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/renato0307/learning-go-lib v0.0.9
 	github.com/rs/zerolog v1.26.1
 	github.com/stretchr/testify v1.7.0
+
+)
+
+require (
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.0-20210816181553-5444fa50b93d // indirect
+	github.com/goccy/go-json v0.8.1 // indirect
+	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.0 // indirect
+	github.com/lestrrat-go/httpcc v1.0.0 // indirect
+	github.com/lestrrat-go/iter v1.0.1 // indirect
+	github.com/lestrrat-go/option v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-playground/locales v0.13.0 // indirect
-	github.com/go-playground/universal-translator v0.17.0 // indirect
-	github.com/go-playground/validator/v10 v10.4.1 // indirect
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/json-iterator/go v1.1.9 // indirect
-	github.com/leodido/go-urn v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
-	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/lestrrat-go/jwx v1.2.14
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.1.0 // indirect
-	github.com/ugorji/go/codec v1.1.7 // indirect
+	github.com/stretchr/objx v0.3.0 // indirect
+	github.com/ugorji/go/codec v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20211215165025-cf75a172585e // indirect
-	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- internal/middleware/authenticator_test.go
+++ internal/middleware/authenticator_test.go
@@ -1,11 +1,18 @@
 package middleware
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/assert/v2"
+	"github.com/lestrrat-go/jwx/jwa"
+	"github.com/lestrrat-go/jwx/jwk"
+	"github.com/lestrrat-go/jwx/jwt"
 	"github.com/renato0307/learning-go-api/internal/apierror"
 	"github.com/renato0307/learning-go-api/internal/apitesting"
 )
@@ -14,7 +21,7 @@ func TestAuthenticatorNoAuthHeader(t *testing.T) {
 
 	// arrange - init gin to use the structured logger middleware
 	r := gin.New()
-	r.Use(Authenticator())
+	r.Use(Authenticator([]byte{}))
 	r.Use(gin.Recovery())
 
 	// arrange - set the routes
@@ -28,11 +35,20 @@ func TestAuthenticatorNoAuthHeader(t *testing.T) {
 	apierror.AssertIsValid(t, w.Body.Bytes())
 }
 
-func TestAuthenticatorInvalidJwt(t *testing.T) {
+func TestAuthenticatorWithInvalidJwt(t *testing.T) {
+	key, err := generateKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	jwks, err := generateKeySetInJSON(&key)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	// arrange - init gin to use the structured logger middleware
 	r := gin.New()
-	r.Use(Authenticator())
+	r.Use(Authenticator(jwks))
 	r.Use(gin.Recovery())
 
 	// arrange - set the routes
@@ -40,7 +56,7 @@ func TestAuthenticatorInvalidJwt(t *testing.T) {
 
 	// arrange - headers
 	header := http.Header{}
-	header.Add("Authentication", "InvalidJWT")
+	header.Add("Authentication", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
 
 	// act
 	w := apitesting.PerformRequestWithHeader(r, "GET", "/example?a=100", header)
@@ -48,4 +64,85 @@ func TestAuthenticatorInvalidJwt(t *testing.T) {
 	// assert
 	assert.Equal(t, http.StatusUnauthorized, w.Code)
 	apierror.AssertIsValid(t, w.Body.Bytes())
+}
+
+func TestAuthenticatorWithValidJwt(t *testing.T) {
+
+	key, err := generateKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	token := jwt.New()
+	token.Set("sub", "client_id_1234567890")
+	token.Set("token_use", "access")
+	token.Set("scope", "https://learning-go-api.com/all")
+	token.Set("auth_time", 1641417382)
+	token.Set("iss", "https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_b9CEhR3UR")
+	token.Set("exp", 1641420982)
+	token.Set("iat", 1641417382)
+	token.Set("version", 2)
+	token.Set("jti", "a6dd28cc-500e-4b49-a510-efda5195d2f4")
+	token.Set("client_id", "client_id_1234567890")
+
+	signed, err := jwt.Sign(token, jwa.RS256, key)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	jwks, err := generateKeySetInJSON(&key)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// arrange - init gin to use the structured logger middleware
+	r := gin.New()
+	r.Use(Authenticator(jwks))
+	r.Use(gin.Recovery())
+
+	// arrange - set the routes
+	r.GET("/example", func(c *gin.Context) {})
+
+	// arrange - headers
+	header := http.Header{}
+	header.Add("Authentication", string(signed))
+
+	// act
+	w := apitesting.PerformRequestWithHeader(r, "GET", "/example?a=100", header)
+
+	// assert
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func generateKey() (jwk.Key, error) {
+	raw, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate new RSA private key: %s\n", err)
+	}
+
+	key, err := jwk.New(raw)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create symmetric key: %s\n", err)
+	}
+	if _, ok := key.(jwk.RSAPrivateKey); !ok {
+		return nil, fmt.Errorf("expected jwk.SymmetricKey, got %T\n", key)
+	}
+
+	key.Set(jwk.KeyIDKey, "mykey")
+
+	return key, nil
+}
+
+func generateKeySetInJSON(key *jwk.Key) ([]byte, error) {
+	set := jwk.NewSet()
+	pubKey, _ := (*key).(jwk.RSAPrivateKey).PublicKey()
+	pubKey.Set(jwk.AlgorithmKey, "RS256")
+	set.Add(pubKey)
+
+	buf, err := json.MarshalIndent(set, "", "  ")
+	if err != nil {
+		return []byte{}, fmt.Errorf("failed to marshal key into JSON: %s\n", err)
+	}
+
+	return buf, nil
 }
